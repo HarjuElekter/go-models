3c9d0b5aa7ec597d46e35e4bb0cab4940aadbf3d
Dainius Matusevicius <dainius.matusevicius@harjuelekter.com>
2023-10-16 13:33:29 +0300

    Removed unused fields

--- mobilSessions.go
+++ mobilSessions.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"strings"
 	"time"
 
 	"github.com/dainiauskas/go-log"
@@ -8,18 +9,13 @@ import (
 )
 
 type MobileSession struct {
-	ID                 int64      `gorm:"primaryKey"`
-	SessionID          string     `gorm:"uniqueIndex" json:"sessionID"`
-	UserName           string     `gorm:"" json:"username"`
-	LoginTime          time.Time  `json:"loginTime"`
-	LogoutTime         *time.Time `json:"logoutTime"`
-	BrowserType        string     `gorm:"size:250" json:"browserType"`
-	HostName           string     `gorm:"size:50"`
-	NumberOfActivities int64      `json:"noOfActivities"`
-	NumberOfParameters int64      `json:"noOfParameters"`
-	OperInsert         int64      `json:"operInsert"`
-	OperDelete         int64      `json:"operDelete"`
-	OperModify         int64      `json:"operModify"`
+	ID          int64      `gorm:"primaryKey"`
+	SessionID   string     `gorm:"uniqueIndex" json:"sessionID"`
+	UserName    string     `gorm:"" json:"username"`
+	LoginTime   time.Time  `json:"loginTime"`
+	LogoutTime  *time.Time `json:"logoutTime"`
+	BrowserType string     `gorm:"size:250" json:"browserType"`
+	HostName    string     `gorm:"size:50"`
 }
 
 func (MobileSession) TableName() string {
@@ -28,6 +24,7 @@ func (MobileSession) TableName() string {
 
 func (m *MobileSession) BeforeCreate(tx *gorm.DB) (err error) {
 	m.LoginTime = time.Now()
+	m.UserName = strings.ToUpper(m.UserName)
 
 	return
 }
