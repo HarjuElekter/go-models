62b1d5fe930c04ace84fa97218ea7e384d594c98
Dainius Matusevicius <dainius.matusevicius@harjuelekter.com>
2023-11-14 09:32:59 +0200

    Changed pointers

--- wmsPickDocument.go
+++ wmsPickDocument.go
@@ -5,17 +5,17 @@ import (
 )
 
 type PickDocument struct {
-	ID         uint        `gorm:"primaryKey"`
-	No         string      `gorm:"size:20;index"`
-	Transfer   string      `gorm:"size:20;index"`
-	Production string      `gorm:"size:20;index"`
-	Project    string      `gorm:"size:20;index"`
-	StartDate  *types.Date `gorm:"type:date"`
-	EndDate    *types.Date `gorm:"type:date"`
-	DueDate    *types.Date `gorm:"type:date"`
-	Lines      uint        ``
-	Assigned   uint        ``
-	Users      []PickUser
+	ID         uint       `gorm:"primaryKey"`
+	No         string     `gorm:"size:20;index"`
+	Transfer   string     `gorm:"size:20;index"`
+	Production string     `gorm:"size:20;index"`
+	Project    string     `gorm:"size:20;index"`
+	StartDate  types.Date `gorm:"type:date"`
+	EndDate    types.Date `gorm:"type:date"`
+	DueDate    types.Date `gorm:"type:date"`
+	Lines      uint       ``
+	Assigned   uint       ``
+	Users      []*PickUser
 }
 
 func (PickDocument) TableName() string {
