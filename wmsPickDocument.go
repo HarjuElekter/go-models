baa409d92c0582c793ea1f47269546f9cf176f2a
Dainius Matusevicius <dainius.matusevicius@harjuelekter.com>
2023-11-14 09:29:05 +0200

    Fix dates

--- wmsPickDocument.go
+++ wmsPickDocument.go
@@ -1,22 +1,20 @@
 package model
 
 import (
-	"time"
-
 	"github.com/dainiauskas/go-types"
 )
 
 type PickDocument struct {
-	ID         uint       `gorm:"primaryKey"`
-	No         string     `gorm:"size:20;index"`
-	Transfer   string     `gorm:"size:20;index"`
-	Production string     `gorm:"size:20;index"`
-	Project    string     `gorm:"size:20;index"`
-	Startdate  types.Date `gorm:"type:date"`
-	EndDate    types.Date `gorm:"type:date"`
-	DueDate    *time.Time `grom:"type:date"`
-	Lines      uint       ``
-	Assigned   uint       ``
+	ID         uint        `gorm:"primaryKey"`
+	No         string      `gorm:"size:20;index"`
+	Transfer   string      `gorm:"size:20;index"`
+	Production string      `gorm:"size:20;index"`
+	Project    string      `gorm:"size:20;index"`
+	StartDate  *types.Date `gorm:"type:date"`
+	EndDate    *types.Date `gorm:"type:date"`
+	DueDate    *types.Date `gorm:"type:date"`
+	Lines      uint        ``
+	Assigned   uint        ``
 	Users      []PickUser
 }
 
