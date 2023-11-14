0d0fb98a7a2db4f9a4d8920eb9ccef35c0d2ee53
Dainius Matusevicius <dainius.matusevicius@harjuelekter.com>
2023-11-14 08:37:48 +0200

    Changed to types Date

--- wmsPickDocument.go
+++ wmsPickDocument.go
@@ -1,18 +1,20 @@
 package model
 
-import "time"
+import (
+	"github.com/dainiauskas/go-types"
+)
 
 type PickDocument struct {
-	ID         uint      `gorm:"primaryKey"`
-	No         string    `gorm:"size:20;index"`
-	Transfer   string    `gorm:"size:20;index"`
-	Production string    `gorm:"size:20;index"`
-	Project    string    `gorm:"size:20;index"`
-	Startdate  time.Time `gorm:"type:date"`
-	EndDate    time.Time `gorm:"type:date"`
-	DueDate    time.Time `grom:"type:date"`
-	Lines      uint      ``
-	Assigned   uint      ``
+	ID         uint       `gorm:"primaryKey"`
+	No         string     `gorm:"size:20;index"`
+	Transfer   string     `gorm:"size:20;index"`
+	Production string     `gorm:"size:20;index"`
+	Project    string     `gorm:"size:20;index"`
+	Startdate  types.Date `gorm:"type:date"`
+	EndDate    types.Date `gorm:"type:date"`
+	DueDate    types.Date `grom:"type:date"`
+	Lines      uint       ``
+	Assigned   uint       ``
 	Users      []PickUser
 }
 
