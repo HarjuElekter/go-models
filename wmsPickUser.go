af353e93dacbe326fed24fa8116a062ae24d833a
Dainius Matusevicius <dainius.matusevicius@harjuelekter.com>
2023-11-15 10:33:42 +0200

    Added json tags

--- wmsPickUser.go
+++ wmsPickUser.go
@@ -3,9 +3,9 @@ package model
 import "github.com/dainiauskas/go-types"
 
 type PickUser struct {
-	Date           types.Date `gorm:"type:date;primaryKey"`
-	UserName       string     `gorm:"size:50;primaryKey"`
-	Scanned        uint       ``
+	Date           types.Date `gorm:"type:date;primaryKey" json:"-"`
+	UserName       string     `gorm:"size:50;primaryKey" json:"username"`
+	Scanned        uint       `json:"lines"`
 	PickDocumentID uint       `gorm:"primaryKey"`
 }
 
