4f140725be3a0feabf53acf0f9da33c0960c0784
Dainius Matusevicius <dainius.matusevicius@harjuelekter.com>
2023-11-15 10:33:51 +0200

    Added constraint

--- wmsPickDocument.go
+++ wmsPickDocument.go
@@ -7,17 +7,17 @@ import (
 )
 
 type PickDocument struct {
-	ID         uint       `gorm:"primaryKey"`
-	No         string     `gorm:"size:20;index"`
-	Transfer   string     `gorm:"size:20;index"`
-	Production string     `gorm:"size:20;index"`
-	Project    string     `gorm:"size:20;index"`
-	Start      time.Time  `gorm:"type:datetime"`
-	End        *time.Time `gorm:"type:datetime"`
-	DueDate    types.Date `gorm:"type:date"`
-	Lines      uint       ``
-	Assigned   uint       ``
-	Users      []*PickUser
+	ID         uint        `gorm:"primaryKey"`
+	No         string      `gorm:"size:20;index"`
+	Transfer   string      `gorm:"size:20;index"`
+	Production string      `gorm:"size:20;index"`
+	Project    string      `gorm:"size:20;index"`
+	Start      time.Time   `gorm:"type:datetime"`
+	End        *time.Time  `gorm:"type:datetime"`
+	DueDate    types.Date  `gorm:"type:date"`
+	Lines      uint        ``
+	Assigned   uint        ``
+	Users      []*PickUser `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 }
 
 func (PickDocument) TableName() string {
