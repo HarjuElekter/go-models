59a2f1b3521081b36ca682ece828ce0c1697696e
Dainius Matusevicius <dainius.matusevicius@harjuelekter.com>
2023-12-08 11:47:43 +0200

    Added production BOM log

---
+++ productionBOM.go
@@ -0,0 +1,18 @@
+package model
+
+import "time"
+
+type ProductionBOMLog struct {
+	ID          uint       `gorm:"primaryKey"`
+	Type        uint8      `json:"type"`
+	UserName    string     `json:"username"`
+	BOMNo       string     `gorm:"size:20" json:"bomNo"`
+	ItemNo      string     `gorm:"size:20" json:"item_no"`
+	Quantity    float64    `json:"quantity"`
+	SystemId    string     `gorm:"size:50;index" json:"systemId"`
+	OperationAt *time.Time `gorm:"type:datetime" json:"operationAt"`
+}
+
+func (ProductionBOMLog) TableName() string {
+	return "production_bom_log"
+}
