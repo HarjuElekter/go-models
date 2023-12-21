29b5f3ac215757f40fdc20045dfdd9ca650e53be
Dainius Matusevicius <dainius.matusevicius@harjuelekter.com>
2023-12-21 14:21:31 +0200

    Added query params

--- productionBOM.go
+++ productionBOM.go
@@ -3,15 +3,15 @@ package model
 import "time"
 
 type ProductionBOMLog struct {
-	ID          uint       `gorm:"primaryKey"`
-	Action      uint8      `json:"action"`
-	Type        string     `json:"type"`
-	UserName    string     `json:"username"`
-	BOMNo       string     `gorm:"size:20" json:"bomNo"`
-	ItemNo      string     `gorm:"size:20" json:"itemNo"`
-	Quantity    float64    `json:"quantity"`
-	SystemId    string     `gorm:"size:50;index" json:"systemId"`
-	OperationAt *time.Time `gorm:"type:datetime" json:"operationAt"`
+	ID          uint       `gorm:"primaryKey" json:"id" query:"id" param:"id" header:"id"`
+	Action      uint8      `json:"action" query:"action" param:"action" header:"action"`
+	Type        string     `json:"type" query:"type" param:"type" header:"type"`
+	UserName    string     `json:"username" query:"username" param:"username" header:"username"`
+	BOMNo       string     `gorm:"size:20" json:"bomNo"  query:"bomNo" param:"bomNo" header:"bomNo"`
+	ItemNo      string     `gorm:"size:20" json:"itemNo" query:"itemNo" param:"itemNo" header:"itemNo"`
+	Quantity    float64    `json:"quantity" query:"quantity" param:"quantity" header:"quantity"`
+	SystemId    string     `gorm:"size:50;index" json:"systemId"  query:"systemId" param:"systemId" header:"systemId"`
+	OperationAt *time.Time `gorm:"type:datetime" json:"operationAt"  query:"operationAt" param:"operationAt" header:"operationAt"`
 }
 
 func (ProductionBOMLog) TableName() string {
