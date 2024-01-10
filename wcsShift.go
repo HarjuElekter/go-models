0c9da4c44e5d273a801241493f4721b4e2a921b2
Dainius Matusevicius <dainius.matusevicius@harjuelekter.com>
2024-01-10 09:04:28 +0200

    Production Workshop Shift models

---
+++ wcsShift.go
@@ -0,0 +1,39 @@
+package model
+
+import "time"
+
+// Pamainų sąrašas
+type ProdWorkShift struct {
+	ID            uint   `gorm:"primaryKey"`
+	Name          string `gorm:"size:40;unique"`
+	WCSShiftTimes []ProdWorkShiftTime
+}
+
+// Pamainos grafikas
+type ProdWorkShiftTime struct {
+	ID                uint                    `gorm:"primaryKey"`
+	WCSShiftID        uint                    ``
+	StartTime         string                  `gorm:"size:5"`
+	EndTime           string                  `gorm:"size:5"`
+	ValidFrom         *time.Time              `gorm:"type:date"`
+	ValidTo           *time.Time              `gorm:"type:date"`
+	WCSShiftSchedules []ProdWorkShiftSchedule ``
+}
+
+// Darbuotojų darbo grafikas
+type ProdWorkShiftSchedule struct {
+	ID             uint              `gorm:"primaryKey"`
+	WCSShiftTimeID uint              ``
+	WCSShiftTime   ProdWorkShiftTime ``
+	Date           time.Time         `gorm:"type:date"`
+	EmployeeNo     string            `gorm:"size:20"`
+}
+
+// Darbuotojų darbo registracija
+type ProdWorkShiftRegister struct {
+	ID             uint              `gorm:"PrimaryKey"`
+	WCSShiftTimeID uint              ``
+	WCSShiftTime   ProdWorkShiftTime ``
+	WorkStart      time.Time         `gorm:"type:time"`
+	WorkEnd        *time.Time        `gorm:"type:time"`
+}
