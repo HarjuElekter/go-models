93976f259eded7f65bfffa00ba6267d8accfe656
Dainius Matusevicius <dainius.matusevicius@harjuelekter.com>
2024-01-11 14:03:05 +0200

    Fixed size and set type

--- employee.go
+++ employee.go
@@ -2,7 +2,7 @@ package model
 
 type Employee struct {
 	ID        uint   `gorm:"primaryKey"`
-	No        string `gorm:"length:20"`
-	FirstName string `gorm:"length:30"`
-	LastName  string `gorm:"length:30"`
+	No        string `gorm:"type:varchar;size:20"`
+	FirstName string `gorm:"type:varchar;size:30"`
+	LastName  string `gorm:"type:varchar;size:30"`
 }
