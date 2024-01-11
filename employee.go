7cd7c088ae4f9fd5316983b4ee46d35cfbb226cd
Dainius Matusevicius <dainius.matusevicius@harjuelekter.com>
2024-01-11 14:05:05 +0200

    Removed tag type

--- employee.go
+++ employee.go
@@ -2,7 +2,7 @@ package model
 
 type Employee struct {
 	ID        uint   `gorm:"primaryKey"`
-	No        string `gorm:"type:varchar;size:20"`
-	FirstName string `gorm:"type:varchar;size:30"`
-	LastName  string `gorm:"type:varchar;size:30"`
+	No        string `gorm:"size:20"`
+	FirstName string `gorm:"size:30"`
+	LastName  string `gorm:"size:30"`
 }
