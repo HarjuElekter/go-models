443331b686508c35542f606bf47673d059746145
Dainius Matusevicius <dainius.matusevicius@harjuelekter.com>
2024-01-30 08:35:07 +0200

    Added indexes and field department

--- employee.go
+++ employee.go
@@ -1,8 +1,9 @@
 package model
 
 type Employee struct {
-	ID        uint   `gorm:"primaryKey"`
-	No        string `gorm:"size:20"`
-	FirstName string `gorm:"size:30"`
-	LastName  string `gorm:"size:30"`
+	ID         uint   `gorm:"primaryKey"`
+	No         string `gorm:"size:20;unique"`
+	FirstName  string `gorm:"size:30"`
+	LastName   string `gorm:"size:30"`
+	Department string `gorm:"size:50;index"`
 }
