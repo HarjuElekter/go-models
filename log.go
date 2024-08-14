6cec83ff9511b6fd9bcf6612b54ec6e30a309ba7
Dainius Matusevičius <dainius.matusevicius@harjuelekter.com>
2024-08-14 12:47:26 +0300

    Added OS

--- log.go
+++ log.go
@@ -2,6 +2,8 @@ package model
 
 type Log struct {
 	ID           int    `gorm:"primaryKey" json:"id"`
+	OS           string `gorm:"size:50" json:"os"`
+	OSVersion    string `gorm:"size:254" json:"os_version"`
 	AppName      string `gorm:"size:50" json:"app_name"`
 	ErrorMessage string `gorm:"" json:"error_message"`
 	Stack        string `gorm:"" json:"stack"`
